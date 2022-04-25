e5c5663056e78b979777386ccd37b9d9bca8e66d
pellinegrc <77890183+pellinegrc@users.noreply.github.com>
2022-04-25 11:35:39 -0400

    message

---
+++ backend/src/main/java/edu/brown/cs/student/main/Event.java
@@ -0,0 +1,9 @@
+package edu.brown.cs.student.main;
+
+import java.util.List;
+
+public class Event {
+  private String building;
+  private int time;
+  private List<String> dayOfWeek;
+}
---
+++ backend/src/main/java/edu/brown/cs/student/main/User.java
@@ -0,0 +1,11 @@
+package edu.brown.cs.student.main;
+
+import java.util.List;
+
+public class User {
+  private String userid;
+  private String email;
+  private String key;
+  private List<Event> events;
+  private double waterBottleSize;
+}
