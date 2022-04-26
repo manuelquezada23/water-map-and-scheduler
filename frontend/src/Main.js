2f8a6abe653baaea8f6fec528e40f274e4af7cd8
manuelquezada23 <manuel_quezada@brown.edu>
2022-04-26 00:51:50 -0400

    progress

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './main-pages/Home'
 import About from './main-pages/About'
 import Map from './main-pages/Map'
@@ -8,16 +8,14 @@ import LogIn from './main-pages/LogIn'
 
 function Main() {
     return (
-        // <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/about" element={<About />}></Route>
-                <Route path="/map" element={<Map />}></Route>
-                <Route path="/contact" element={<Contact />}></Route>
-                <Route path="/login" element={<LogIn />}></Route>
-                <Route path="*" element={<PageNotFound />}></Route>
-            </Routes>
-        // </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/map" element={<Map />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/login" element={<LogIn />}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
     );
 }
 
