1dc1bdf688710b7b4ddcc8a5d0202ae801a5156f
manuelquezada23 <manuel_quezada@brown.edu>
2022-04-26 16:36:59 -0400

    map not logged in

--- frontend/src/main-pages/Map.js
+++ frontend/src/main-pages/Map.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import logo from '../logo.png'
+import { useNavigate } from "react-router-dom";
 
-export default class Map extends React.Component {
-    render() {
-      return <h1>Map</h1>;
-    }
+function Map() {
+  const isLoggedIn = false
+  const navigate = useNavigate()
+
+  return (
+    <div className="main-page-body">
+      {(isLoggedIn === false) &&
+        <div className="map-not-loggedin">
+          <img src={logo} className="map-not-loggedin-logo" alt="logo" />
+          <p className="map-not-loggedin-text">Log In or Sign Up to use our map!</p>
+          <button onClick={() => {navigate("/login")}} className="map-not-loggedin-button">Get Started</button>
+        </div>
+      }
+    </div>
+  );
 }
+
+export default Map;
