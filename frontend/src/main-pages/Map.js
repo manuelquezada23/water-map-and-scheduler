2e6fe5347b8aca09a05bae2a01ea935c1f31c3c6
mayafleischer <maya_fleischer@brown.edu>
2022-04-27 15:19:25 -0400

    feat: leave a review popup, not the star rating

--- frontend/src/main-pages/Map.js
+++ frontend/src/main-pages/Map.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import PictureIcon from '../picture.png'
 
-export default class Map extends React.Component {
-    render() {
-      return <h1>Map</h1>;
-    }
+function Map() {
+  return (
+    <div className="main-page-body">
+      <div className='popup-box'>
+        <div className='review-popup'>
+          <p className="building-name">Sciences Library</p>
+          <div className='author-box'>
+            <img className="review-image" src={PictureIcon}></img>
+            <div className="stars">
+              <p className="author">Jane Doe</p>
+              {/* <p className="author">stars</p> */}
+              <p>star rating</p>
+            </div>
+          </div>
+          <textarea className="review-box" placeholder="Type response here" rows="10" type="text" required />
+          <div className="review-submit">
+            <button className="review-submit-button">Post</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Map;
