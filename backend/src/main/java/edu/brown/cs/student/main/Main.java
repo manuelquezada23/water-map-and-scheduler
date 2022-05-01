9700ea34f4500954062c21875b5b326f05090c8a
emilyhinds <emilyhinds217@gmail.com>
2022-05-01 14:11:06 -0400

    func: finds 3 nearest fountains

--- backend/src/main/java/edu/brown/cs/student/main/Main.java
+++ backend/src/main/java/edu/brown/cs/student/main/Main.java
@@ -1,5 +1,14 @@
 package edu.brown.cs.student.main;
 
+import edu.brown.cs.student.main.buildings.Building;
+import edu.brown.cs.student.main.buildings.BuildingCommands;
+import edu.brown.cs.student.main.buildings.Fountain;
+import edu.brown.cs.student.main.buildings.NearestFountain;
+import edu.brown.cs.student.main.database.Database;
+
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * The Main class of our project. This is where execution begins.
  */
@@ -22,6 +31,23 @@ public final class Main {
   }
 
   private void run() {
-    new Api(this.args, "..data/data.sqlite");
+//    new Api(this.args, "..data/test-data.sqlite");
+    try {
+      System.out.println("running");
+      BuildingCommands buildingCommands = new BuildingCommands(new Database("../data/test-data.sqlite"));
+      List<Building> bl = buildingCommands.getBuildings();
+
+      NearestFountain nf = new NearestFountain(bl);
+      List<Fountain> fountainList = nf.findNearestFountains(bl.get(0));
+
+      for (Fountain f : fountainList) {
+        System.out.println(f.getBuildingName() + " " + f.getFloor() + " " + f.getId());
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
   }
 }
