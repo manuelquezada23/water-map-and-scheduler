7dbf664d167d4ae13187616cf1a42276f6be11a3
pellinegrc <77890183+pellinegrc@users.noreply.github.com>
2022-05-01 16:49:23 -0400

    loading in users working

--- backend/src/main/java/edu/brown/cs/student/main/userFunc/userCommands.java
+++ backend/src/main/java/edu/brown/cs/student/main/userFunc/userCommands.java
@@ -5,6 +5,7 @@ import edu.brown.cs.student.main.database.Database;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class userCommands {
@@ -16,33 +17,34 @@ public class userCommands {
     this.users = new ArrayList<User>();
   }
 
-  private void getData() throws SQLException {
-    this.users = null;
+  public void getData() throws SQLException {
+    this.users = new ArrayList<User>();
     ResultSet rs = this.database.executeCommand("SELECT UserID FROM users");
     while (rs.next()) {
       this.users.add(new User(rs.getInt(1)));
     }
     for (int i = 0; i < this.users.size(); i++) {
       int id = this.users.get(i).getUserid();
-      rs = this.database.executeCommand("SELECT Name FROM users WHERE UserID = " + id);
+      rs = this.database.executeCommand("SELECT * FROM users WHERE UserID = " + id);
       while (rs.next()) {
-        this.users.get(i).setName(rs.getString(1));
+        this.users.get(i).setName(rs.getString("Name"));
+        this.users.get(i).setEmail(rs.getString("Email"));
+        this.users.get(i).setKey(rs.getString("Key"));
+        this.users.get(i).setWaterBottleSize(rs.getDouble("WaterBottleSize"));
       }
-      rs = this.database.executeCommand("SELECT Email FROM users WHERE UserID = " + id);
+      List<Event> events = new ArrayList<Event>();
+      rs = this.database.executeCommand("SELECT * FROM events WHERE UserID = " + id);
       while (rs.next()) {
-        this.users.get(i).setEmail(rs.getString(1));
+        String location = rs.getString("BuildingName");
+        int startTime = rs.getInt("StartTime");
+        int endTime = rs.getInt("EndTime");
+        List<String> days = Arrays.asList((rs.getString("DaysOfWeek").split(",")));
+        this.users.get(i).addEvents(new Event(location, startTime, endTime, days));
       }
-      rs = this.database.executeCommand("SELECT WaterBottleSize FROM users WHERE UserID = " + id);
-      while (rs.next()) {
-        this.users.get(i).setWaterBottleSize(rs.getDouble(1));
-      }
-      rs = this.database.executeCommand("SELECT Key FROM users WHERE UserID = " + id);
-      while (rs.next()) {
-        this.users.get(i).setKey(rs.getString(1));
-      }
-      
-      //get list of events from other database 
     }
   }
 
+  public List<User> getUsers() {
+    return this.users;
+  }
 }
--- backend/src/test/java/edu/brown/cs/student/main/A.java
+++
@@ -1,4 +0,0 @@
-package edu.brown.cs.student.main;
-
-public class A {
-}
