990076c982bd152423fd1c3d4f5d224eef4fd992
pellinegrc <77890183+pellinegrc@users.noreply.github.com>
2022-05-01 14:10:32 -0400

    working

--- backend/src/main/java/edu/brown/cs/student/main/userFunc/userCommands.java
+++ backend/src/main/java/edu/brown/cs/student/main/userFunc/userCommands.java
@@ -13,29 +13,30 @@ public class userCommands {
 
   public userCommands(Database database) {
     this.database = database;
-    this.users = new ArrayList<>();
+    this.users = new ArrayList<User>();
   }
 
   private void getData() throws SQLException {
     this.users = null;
-    ResultSet rs = this.database.executeCommand("SELECT id FROM users");
+    ResultSet rs = this.database.executeCommand("SELECT UserID FROM users");
     while (rs.next()) {
       this.users.add(new User(rs.getInt(1)));
     }
     for (int i = 0; i < this.users.size(); i++) {
-      rs = this.database.executeCommand("SELECT name FROM users");
+      int id = this.users.get(i).getUserid();
+      rs = this.database.executeCommand("SELECT Name FROM users");
       while (rs.next()) {
         this.users.get(i).setName(rs.getString(1));
       }
-      rs = this.database.executeCommand("SELECT email FROM users");
+      rs = this.database.executeCommand("SELECT Email FROM users");
       while (rs.next()) {
         this.users.get(i).setEmail(rs.getString(1));
       }
-      rs = this.database.executeCommand("SELECT Water Bottle Size FROM users");
+      rs = this.database.executeCommand("SELECT WaterBottleSize FROM users");
       while (rs.next()) {
         this.users.get(i).setWaterBottleSize(rs.getDouble(1));
       }
-      rs = this.database.executeCommand("SELECT key FROM users");
+      rs = this.database.executeCommand("SELECT Key FROM users");
       while (rs.next()) {
         this.users.get(i).setKey(rs.getString(1));
       }
