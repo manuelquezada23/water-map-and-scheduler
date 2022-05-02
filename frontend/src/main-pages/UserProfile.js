1a4028ac6e9388061440379e7c41de0e48ee775d
manuelquezada23 <manuel_quezada@brown.edu>
2022-05-01 21:09:37 -0400

    more work done

--- frontend/src/main-pages/UserProfile.js
+++ frontend/src/main-pages/UserProfile.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PictureIconLarge from '../picture-large.png'
 import './main-pages.css'
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 function UserProfile() {
     const navigate = useNavigate();
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const userEmail = user.email;
+
     return (
         <div className="main-page-body">
             <div className="user-profile-box">
                 <div className="user-profile">
                     <p className='profile-header'>My Profile</p>
                     <img className="profile-image" src={PictureIconLarge}></img>
-                    <input className='user-input' defaultValue="Emily Hinds" type="text"/>
-                    <input className='user-input' defaultValue="emily_hinds@brown.edu" type="text"/>
+                    <input className='user-input' placeholder="Name" type="text" />
+                    <input className='user-input' placeholder="Email" type="text" disabled value={userEmail}></input>
                     <button className="info-change-button">Save</button>
                 </div>
                 <div className="user-profile">
@@ -28,7 +33,7 @@ function UserProfile() {
                     </div>
                     <button className="info-change-button">Change</button>
                 </div>
-                
+
             </div>
             <div>
                 <button className="profile-edit-save-button">Save</button>
