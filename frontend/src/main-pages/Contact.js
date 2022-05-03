645590b92a2cb9d75cf1a52472490d7b29e9cb96
mayafleischer <maya_fleischer@brown.edu>
2022-05-03 11:12:56 -0400

    contacts form function

--- frontend/src/main-pages/Contact.js
+++ frontend/src/main-pages/Contact.js
@@ -1,24 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function Contact() {
-  return (
-      <div className="main-page-body">
-          <p className="login-header">Have questions?</p>
-          <p className="contact-display-message">Get in touch with our team!</p>
-          <form>
-              <div className="login-input-boxes">
-                  <div className="contact-input-boxes">
-                      <input className="contact-input" placeholder="Name" type="text" required />
-                      <input className="contact-input" placeholder="Email" type="text" required />
-                  </div>
-                  <textarea className="contact-message" placeholder="Type response here" rows="10" type="text" required />
-                  <div className='contact-submit'>
-                    <button className="contact-submit-button">Send</button>
-                  </div>
-              </div>
-          </form>
-      </div>
-  );
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+    const [success, setSuccess] = useState(false)
+
+    const redirectToPage = () => setSuccess(true) ;  
+
+    // {success && (
+    //     <Fragment>
+    //       <Redirect to='/thankyou' />  
+    //     </Fragment>
+    //    )} 
+
+    // if (success) {
+    //     return (
+    //         <div className="main-page-body">
+    //             <p className="login-header">Thank you!</p>
+    //             <p className="contact-display-message">Your message has been sent.</p>
+    //         </div>
+    //     )
+    // } else {
+        return (
+            <div className="main-page-body">
+                <p className="login-header">Have questions?</p>
+                <p className="contact-display-message">Get in touch with our team!</p>
+                <form id='form'>
+                    <div className="login-input-boxes">
+                        <div className="contact-input-boxes">
+                            <input className="contact-input" placeholder="Name" type="text" onChange={(e) => setName(e.target.value)} required />
+                            <input className="contact-input" placeholder="Email" type="email" onChange={(e) => setEmail(e.target.value)} required />
+                        </div>
+                        <textarea className="contact-message" placeholder="Type response here" rows="10" type="text" onChange={(e) => setMessage(e.target.value)} required />
+                        <div className='contact-submit'>
+                            <button className="contact-submit-button" onClick={(e) => {
+                                for (const el of document.getElementById('form').querySelectorAll("[required]")) {
+                                    if (!el.reportValidity()) {
+                                      return;
+                                    }
+                                }
+                                e.preventDefault();
+                                handleContact(name, email, message)
+                            }}>Send</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    // }
+}
+
+function setSuccess() {
+    return (
+        <div className="main-page-body">
+            <p className="login-header">Thank you!</p>
+            <p className="contact-display-message">Your message has been sent.</p>
+        </div>
+    )
+}
+
+function handleContact(name, email, message) {
+    console.log("handled")
+    console.log(name)
+    console.log(email)
+    console.log(message)
+    setSuccess();
+    // Contact.setSuccess(true)
+    
+    // fetch('http://localhost:3000/send-message', {
+    //     method: "POST",
+    //     body: JSON.stringify(name,email,message),
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     }).then(
+    //     (response) => (response.json())
+    //     ).then((response)=> {}
+    //     ).catch((error) => {})
 }
 
 export default Contact;
--- frontend/src/main-pages/Map.js
+++ frontend/src/main-pages/Map.js
@@ -1,28 +1,114 @@
-import React from 'react';
-import PictureIcon from '../picture.png'
+// import { React, useState, useMemo, useCallback, useRef } from "react";
+// import {
+//   GoogleMap,
+//   Marker,
+//   DirectionsRenderer,
+//   Circle,
+//   MarkerClusterer,
+// } from "@react-google-maps/api";
+// import Places from "./places";
+// import Distance from "./distance";
 
-function Map() {
+export default function Map() {
+  // let LatLngLiteral = google.maps.LatLngLiteral;
+  // let DirectionsResult = google.maps.DirectionsResult;
+  // let MapOptions = google.maps.MapOptions;
+  // const center = useMemo<LatLngLiteral>(
+  //   () => ({ lat: 43.45, lng: -80.49 }),
+  //   []
+  // );
+
   return (
     <div className="main-page-body">
-      <div className='popup-box'>
-        <div className='review-popup'>
-          <p className="building-name">Sciences Library</p>
-          <div className='author-box'>
-            <img className="review-image" src={PictureIcon}></img>
-            <div className="stars">
-              <p className="author">Jane Doe</p>
-              {/* <p className="author">stars</p> */}
-              <p>star rating</p>
-            </div>
-          </div>
-          <textarea className="review-box" placeholder="What did you think?" type="text" required />
-          <div className="review-submit">
-            <button className="review-submit-button">Post</button>
-          </div>
-        </div>
-      </div>
+      <h1>map here</h1>
     </div>
   );
 }
 
-export default Map;
+// export default function Map() {
+//   let LatLngLiteral = google.maps.LatLngLiteral;
+//   let DirectionsResult = google.maps.DirectionsResult;
+//   let MapOptions = google.maps.MapOptions;
+//   const center = useMemo<LatLngLiteral>(
+//     () => ({ lat: 43.45, lng: -80.49 }),
+//     []
+//   );
+
+//   return (
+//     <div className="container">
+//       <div className="controls">
+//         <h1>Commute?</h1>
+//         <Places
+//           setOffice={(position) => {
+//             setOffice(position);
+//             mapRef.current?.panTo(position);
+//           }}
+//         />
+//         {!office && <p>Enter the address of your office.</p>}
+//         {directions && <Distance leg={directions.routes[0].legs[0]} />}
+//       </div>
+//       <div className="map">
+//         <GoogleMap
+//           zoom={10}
+//           center={center}
+//           mapContainerClassName="map-container"
+//           options={options}
+//           onLoad={onLoad}
+//         >
+//           {directions && (
+//             <DirectionsRenderer
+//               directions={directions}
+//               options={{
+//                 polylineOptions: {
+//                   zIndex: 50,
+//                   strokeColor: "#1976D2",
+//                   strokeWeight: 5,
+//                 },
+//               }}
+//             />
+//           )}
+
+//           {office && (
+//             <>
+//               <Marker
+//                 position={office}
+//                 icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+//               />
+
+//               <MarkerClusterer>
+//                 {(clusterer) =>
+//                   houses.map((house) => (
+//                     <Marker
+//                       key={house.lat}
+//                       position={house}
+//                       clusterer={clusterer}
+//                       onClick={() => {
+//                         fetchDirections(house);
+//                       }}
+//                     />
+//                   ))
+//                 }
+//               </MarkerClusterer>
+
+//               <Circle center={office} radius={15000} options={closeOptions} />
+//               <Circle center={office} radius={30000} options={middleOptions} />
+//               <Circle center={office} radius={45000} options={farOptions} />
+//             </>
+//           )}
+//         </GoogleMap>
+//       </div>
+//     </div>
+//   );
+// }
+
+// // const generateHouses = (position: google.maps.LatLngLiteral) => {
+// //   const _houses: Array<LatLngLiteral> = [];
+// //   for (let i = 0; i < 100; i++) {
+// //     const direction = Math.random() < 0.5 ? -2 : 2;
+// //     _houses.push({
+// //       lat: position.lat + Math.random() / direction,
+// //       lng: position.lng + Math.random() / direction,
+// //     });
+// //   }
+// //   return _houses;
+// // };
