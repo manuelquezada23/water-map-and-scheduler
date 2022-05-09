6a1f3723297bce3463602f739b235a4262410cb7
mayafleischer <maya_fleischer@brown.edu>
2022-05-09 10:59:26 -0400

    merge backend

---
+++ backend/src/main/java/edu/brown/cs/student/main/userFunc/User 2.java
@@ -0,0 +1,52 @@
+package edu.brown.cs.student.main.userFunc;
+
+import java.util.List;
+
+public class User {
+  private int userid;
+  private String email;
+  private String key;
+  private String name;
+  private List<Event> events;
+  private double waterBottleSize;
+
+  public User(int userid) {
+    this.userid = userid;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getKey() {
+    return this.key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public double getWaterBottleSize() {
+    return this.waterBottleSize;
+  }
+
+  public void setWaterBottleSize(double waterBottleSize) {
+    this.waterBottleSize = waterBottleSize;
+  }
+
+  public int getUserid() {
+    return this.userid;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ frontend/src/main-pages/PageNotFound 2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../logo.png'
+import { useNavigate } from "react-router-dom";
+
+function PageNotFound() {
+  const navigate = useNavigate()
+
+  return (
+    <div className="login">
+      <img onClick={() => {
+        navigate("/")
+      }} src={logo} className="pagenotfound-logo" alt="logo" />
+      <p className="login-header">404</p>
+      <p className="pagenotfound-text">Page Not Found :(</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
